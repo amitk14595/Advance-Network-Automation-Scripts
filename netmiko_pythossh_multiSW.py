09bb9985b9128c9dd7fc464312dbd654c2092105
Amit Kumar <singh.amit14595@gmail.com>
2020-04-17 03:30:07 +0530

    Add files via upload

---
+++ netmiko_pythossh_multiSW.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.71',
+    'username': 'amit',
+    'password': 'cisco',
+}
+
+iosv_l2_s2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': 'amit',
+    'password': 'cisco',
+}
+
+iosv_l2_s3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.73',
+    'username': 'amit',
+    'password': 'cisco',
+}
+
+iosv_l2_s4 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.74',
+    'username': 'amit',
+    'password': 'cisco',
+}
+
+iosv_l2_s5 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.75',
+    'username': 'amit',
+    'password': 'cisco',
+}
+
+with open('iosv_l2_config') as f:
+    lines = f.read().splitlines()
+print lines
+
+all_devices = [iosv_l2_s5, iosv_l2_s4, iosv_l2_s3, iosv_l2_s2, iosv_l2_s1]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print output
